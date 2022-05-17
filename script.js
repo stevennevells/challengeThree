e201cbffa8621310760594a66437481720a884f3
Steven Nevells <stevennevells1795@gmail.com>
2022-05-17 00:49:45 -0700

    added content to generate password
    mods for js

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-const lowercase = ["q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m"];
-const uppercase = ["Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M"];
-const numbers = ["1,2,3,4,5,6,7,8,9,0"];
-const characters = ["!,(,),*,+,-,/,:,;,<,=,>,?,@,[,\,],^,_,`,{,|,},~"];
+const lowercase = ["qwertyuiopasdfghjklzxcvbnm"];
+const uppercase = ["QWERTYUIOPASDFGHJKLZXCVBNM"];
+const numbers = ["1234567890"];
+const characters = ["!()*+-/:;<=>?@[\]^_`{|}"];
+
+var str = lowercase + uppercase + numbers + characters;
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
-
+    
 }
+
+
+
 
 function generatePassword() {
   console.log("click generate password");
 
   prompt("Choose a number between 8 and 128 for your password length");
   if (true) 
-    prompt("Would you like to include uppercase letters?");
+    confirm("Would you like to include uppercase letters?");
     if (true) {
-      prompt("Would you like to include numbers?"); 
+      confirm("Would you like to include numbers?"); 
       if (true) {
-        prompt("Would you like to include spceial characters?");
+        confirm("Would you like to include spceial characters?");
         if (true) {
-          prompt("Would you like to include lowercase numbers?");
+          confirm("Would you like to include lowercase numbers?");
         }
 
-  return "Your new password is " + lowercase + uppercase + numbers+ characters; 
+  return ("Your new password is " + str); 
 } } }
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
+
 
 
   
